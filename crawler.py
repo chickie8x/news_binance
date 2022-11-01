9cfdee3b3e306f5e1e29453fb9821dd234d62f44
chickie8x <chickie8x.qn@gmail.com>
2022-11-01 23:44:33 +0700

    change API endpoint

--- crawler.py
+++ crawler.py
@@ -3,18 +3,15 @@ import time
 from datetime import datetime
 from dotenv import dotenv_values
 import requests
+from slugify import slugify
 
 # init states
 config = dict(dotenv_values('.env'))
-first_run_flag = True
-pages = 6
-pageNo = 1
-count = 1
+url = config['BASE_URL']
 post_url = config["POST_URL"]
-news_url = config["NEWS_LINK"]
+record_url = config["RECORD_URL"]
 
 while True:
-    print(count)
     saved_id = []
     list_news = []
     data_write = {}
@@ -23,32 +20,29 @@ while True:
         read_data = file.read()
         json_data = json.loads(read_data)
         saved_id = json_data["savedId"]
-        list_news = json_data["news"]
+        exist_news = json_data["record"]
+        new_fetch =[]
 
-    if not first_run_flag:
-        pages = 1
-    for i in range(1, pages + 1):
-        url = config['BASE_URL'] + str(i) + config['API_CONF']
-        data = requests.get(url)
-        jdata = json.loads(data.text)
-        for item in jdata["data"]["contents"]:
-            obj = {}
-            if item["id"] not in saved_id:
-                new_fetch.append(item["id"])
-                saved_id.append(item["id"])
-                obj["id"] = item["id"]
-                obj["title"] = item["title"]
-                obj["content"] = item["body"]
-                obj["date_created"] = str(datetime.fromtimestamp((int(item["createTime"])) / 1000))
-                obj["url"] = news_url + str(item["id"])
-                r = requests.post(post_url, data=obj)
-                print(r.status_code)
-                list_news.append(obj)
+    data = requests.get(url)
+    jdata = json.loads(data.text)
+    for item in jdata["data"]["catalogs"]:
+        if item["catalogId"] == 49:
+            if item["articles"].__len__():
+                for record in item["articles"]:
+                    if record["id"] not in saved_id:
+                        obj = {}
+                        saved_id.append(record["id"])
+                        obj["title"] = record["title"]
+                        print(slugify(record["title"]))
+                        obj["url"] = record_url + slugify(record["title"]) + "-" + str(record["code"]) + "_nguyentran"
+                        obj["published_date"] = str(datetime.fromtimestamp(int(record["releaseDate"])//1000))
+                        new_fetch.append(obj)
+                if new_fetch.__len__():
+                    r = requests.post(post_url, json=new_fetch)
+                    print(r.status_code)
     data_write["savedId"] = saved_id
-    data_write["news"] = list_news
+    data_write["record"] = exist_news + new_fetch
     with open('data.json', 'w', encoding='utf') as file:
         json.dump(data_write, file, indent=4, ensure_ascii=False)
-    new_fetch = []
-    first_run_flag = False
-    count += 1
+
     time.sleep(10)
